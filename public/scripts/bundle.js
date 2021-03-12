1277fc231e4f475c5bd7536dd2745003fe51705b
Eric <eric_baffour-addo@alumni.brown.edu>
2021-03-12 15:54:36 -0800

    finished find longest substring

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -15721,33 +15721,48 @@ module.exports = g;
 "use strict";
 
 
-// we are writing a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is zero and retrun the array that includes both values that sum to zero or undefined if a pair does not exist.
+// write a function called findLongestSubstring which accepts a string and return sthe length of the longest consective substring with all distinct characters;
+// ex: thecatinthehat=>7 because hecatin is the longest consecutive substring of unique characters.
 
-//ex: [-3, -2, -1, 0, 1, 2, 3];
+var findLongestSubstring = function findLongestSubstring(string) {
+  if (!string.length) {
+    return 0;
+  }
+  // so from the getgo,  our uniqueString variable, which keeps track of our unique letters is empty.
+  var uniqueString = "";
 
-// so since this is a sorted array. we can have two points. one starts at the first number and the other starts at the last number. if the sum is greater than 0, we cant move the first pointer up, because that is creating a bigger number, so we have to move the second pointer downn 1. If the sum is less than 0, we cant move the second pointer down 1, because that is creating a smaller number,so we have to move the first pointer up one
+  //  current maxLength of our unique letters is 0
+  var maxLength = 0;
 
-var isSubsequence = function isSubsequence(string1, string2) {
-  var string1New = string1.split(" ").join("");
-  var string2New = string2.split(" ").join("");
-  var i = 0;
-  var j = 0;
+  // the startWindow is the 0 index;
+  var startWindow = 0;
 
-  while (i < string1New.length) {
-    if (string1New[i] === string2New[j]) {
-      i++;
-      j++;
-    } else if (string1New[i] !== string2New[j]) {
-      if (!string2New[j]) {
-        return false;
-      }
-      j++;
+  // the endWindow is also the 0 index. Our while loop will begin with trying to see if this 0 index is unque.
+  var endWindow = 0;
+
+  while (startWindow < string.length && endWindow < string.length) {
+    // if our uniqueString variable doesn't include the value where our endWindow is at in our string variable, then we add this value to our uniqueString variable, because it is a uniqueValue. Also, if this is the first item in our uniqueString vairable, we set its maxLength equal to 1. We don't keep setting it equal to 1 when we have a uniqueString, becuase we And, then we calculate the new consecutive maxLength of the unique letters in our uniqueString. And then we go ahead and increase our endWindow by 1 so our while loop can check to see if the new window created results in a longer substring of distinct characters than before;
+    if (!uniqueString.includes(string[endWindow])) {
+      uniqueString += string[endWindow];
+
+      // the new maxLength is either equal to our current maxLength or equal to the length of our new window, which is endWindow-startWindow and then we add 1 so we can get length, otherwise we are dealing with index
+      maxLength = Math.max(maxLength, endWindow - startWindow + 1);
+
+      // then we increase our endWindow by 1, so when our while loop runs again it can check to see if this new window contains a longer length of consecutive unique letter
+      endWindow += 1;
+
+      // if uniqueString does include the letter that our endWindow is at, it means our uniqueString has already reached its current maximum number of consecutive uniqueLetters. So, we increase our startWindow by 1, make our endWindow equal to our startWindow plus 1 make our unqueString variable just has the value of our startWindow in there, and then make our endWindow equal to our startWindow. This way, we are creating a whole new window just shifted up 1 from our original startWindw to find the new longest length of consecutive unique letter. And we also have to make our uniqueString empty again if our first uniqueString was the first 6 letters and the seventh letter wasnt unique, then we would start from the second letter and find how many unique letters after that and over and over.
+    } else if (uniqueString.includes(string[endWindow])) {
+      startWindow += 1;
+      endWindow = startWindow;
+      uniqueString = "";
     }
   }
-  return true;
+
+  return maxLength;
 };
 
-console.log(isSubsequence("abc", "acb"));
+console.log(findLongestSubstring("rithmschool"));
 
 /***/ }),
 
--- source/Udemy/patterns/sliding_window/maxSubArraySum.js
+++ source/Udemy/patterns/sliding_window/maxSubArraySum.js
@@ -7,13 +7,26 @@ const maxSubArraySum = (array, consecutiveNumber) => {
   let tempSum = 0;
 
   // so if the length of the array is less than the number of consecutive numbers that we want added together, we can already return undefined, because not possible.
-  if (array.length < consectuveNumber) {
+  if (array.length < consecutiveNumber) {
     return undefined;
   }
-  let i = 0;
-  let j = i + 1;
 
-  while (j < array.length) {
-    66;
+  // looking at our example to do this problem, we start by summing the first two numbers(since that is what our consecutiveNumber value is) and store that as our maxSum
+  for (let i = 0; i < consecutiveNumber; i++) {
+    maxSum += array[i];
   }
+
+  //so initially, tempSum is equal to maxSum
+  tempSum = maxSum;
+
+  // now we start a loop from the third number(if theyre looking for 2 consecutive numbers), because we already have the sum of the first two numbers.
+  for (let i = consecutiveNumber; i < array.length; i++) {
+    // so, we already have 1+2= 3 as our max and tempSum
+    // now the new sum we want begins at 5, which is the secondIndex(and also ends up being the value for consecutiveNumber) so i=num and we will run this loop till the end of our array.
+    // what we do in the loop is to find the new tempSum, we look at the current tempSum and if we're currently at the third integer, we subtract the very first integer, which is in our case 1 and we get that by doing array[i-num] which initially is array[0], which is the first item in our array and we add the current integer were on. so array[i], so we add 2+5 and we get 7, for the next consecutive sum of two integers
+    tempSum = tempSum - array[i - consecutiveNumber] + array[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
 };
