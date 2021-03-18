b02f5fe7a7fc08c7f651a031652f8d9ea00b59bf
Eric <eric_baffour-addo@alumni.brown.edu>
2021-03-18 01:15:27 -0700

    working on minLength ssubstring

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -15721,76 +15721,56 @@ module.exports = g;
 "use strict";
 
 
-/*
-Given a string and a pattern, find all anagrams of the pattern in the given string.
-
-Anagram is actually a Permutation of a string. For example, “abc” has the following six anagrams:
-
-abc
-acb
-bac
-bca
-cab
-cba
-Write a function to return a list of starting indices of the anagrams of the pattern in the given string.
-
-Example 1:
-
-Input: String="ppqp", Pattern="pq"
-Output: [1, 2]
-Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
-Example 2:
-
-Input: String="abbcabc", Pattern="abc"
-Output: [2, 3, 4]
-Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
-*/
-
-var returnIndexAnagram = function returnIndexAnagram(string, pattern) {
+var smallestWindowSubstring = function smallestWindowSubstring(string, pattern) {
+  var minLength = Infinity;
   var startWindow = 0;
-  var resultArray = [];
-  var patternsFrequency = {};
-  var keyMatch = 0;
+  var frequencyMatch = 0;
+  var subStart = 0; // keeps track of which index our min substring starts
+  var patternFrequencyObject = {};
 
   for (var i = 0; i < pattern.length; i++) {
     var letter = pattern[i];
-    if (!(letter in patternsFrequency)) {
-      patternsFrequency[letter] = 1;
+    if (letter in patternFrequencyObject) {
+      patternFrequencyObject[letter] += 1;
     } else {
-      patternsFrequency[letter] += 1;
+      patternFrequencyObject[letter] = 1;
     }
   }
 
   for (var endWindow = 0; endWindow < string.length; endWindow++) {
     var _letter = string[endWindow];
-    if (_letter in patternsFrequency) {
-      patternsFrequency[_letter] -= 1;
-      if (patternsFrequency[_letter] === 0) {
-        keyMatch += 1;
+    if (_letter in patternFrequencyObject) {
+      patternFrequencyObject[_letter] -= 1;
+      //we count every matching of a character, not only when all the frequencies of a key have been matched. So in this case, if patternFrequencyObject has 5 a's, and if the first letter is an a, then we only have 4 more a's to match. But, we will increment our frequencyMatch, meaning we matched one letter already. We only do this if the frequency of the letter is >=0. It can be less than 0 though, because the substring just needs to have all the characters of the given patter, but it can have more though
+      if (patternFrequencyObject[_letter] >= 0) {
+        frequencyMatch += 1;
       }
     }
 
-    // if the length of our window is equal to the pattern length, that means we check to see if a permuation exists, and this is checked when we compare the length of our keyMatch, the numbers of keys we have fully accounted for and the number of keys in our patternsFrequency. if they are equal, we have a permuatation. Now after we check, if there is a permuation, great. we shrink our window to see if we can find another substring. if no permutation, then obvs, one or more letters in this substring is not part of the pattern, so we shrink our substring again.
-    if (endWindow - startWindow + 1 === pattern.length) {
-      if (keyMatch === Object.keys(patternsFrequency).length) {
-        resultArray.push(startWindow);
-      }
+    // so now, if the frequencyMatch, which just tells us how many characters we have matched is equal to the length of the pattern. so if the pattern is "abc", the length is 3 and if we have 2 a's in our string, after the first a, we decrement our partternFrequencyObject[letter] and its result is 0, so we increment our frequencyMatch, meaning we have maatched one a. But the second a, after we decrement our patternFrequencyObject[letter] the result is negative, because there are more a's in our string than pattern. Since, its negative, we dont increment our frequencyMatch. while this is equal, our condition is met, so we find the min Length and then shrink our window.
+    while (frequencyMatch === pattern.length) {
+      minLength = Math.min(minLength, endWindow - startWindow + 1);
+      subStart = startWindow; // our subStart begins where our startWindow is
       var startLetter = string[startWindow];
       startWindow += 1;
-      if (startLetter in patternsFrequency) {
-        if (patternsFrequency[startLetter] === 0) {
-          keyMatch -= 1;
+      // if the letter leaving our window is inside our patternFrequencyObject. we will decrement the matched count ofnly if the frequency of the character is 0 in our patternFrequencyObject, meaning it is a useful character. because, the thing is we could have redudant matchning characters, so we dont want to decrement all the time, because we're not sure. And we only need  1 charcter  from our pattern to appear in our string
+      if (startLetter in patternFrequencyObject) {
+        if (patternFrequencyObject[startLetter] === 0) {
+          frequencyMatch -= 1;
         }
-        patternsFrequency[startLetter] += 1;
+        // we will also increment the startLetter in patternfrequencyObject. the way our code is set up, as long as an instance of a letter is matched, we wil increase our frequencyMatch
+        patternFrequencyObject[startLetter] += 1;
       }
     }
   }
-
-  //in the end , we want to return our results array
-  return resultArray;
+  // if our minimum length exceeds the size of our string,then obviously, we dont have a substring inside our string that works.
+  if (minLength > string.length) {
+    return "";
+  }
+  return string.substring(subStart, subStart + minLength);
 };
 
-console.log(returnIndexAnagram("abbcabc", "abc"));
+console.log(smallestWindowSubstring("aabdec", "abc"));
 
 /***/ }),
 
