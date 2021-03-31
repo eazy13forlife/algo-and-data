7858b33a04827fdfc7d784d9244abf6e2ae133f9
Eric <eric_baffour-addo@alumni.brown.edu>
2021-03-30 18:03:11 -0700

    finished sumzero

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -15722,38 +15722,82 @@ module.exports = g;
 
 
 /*
-Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+
+Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
 Example 1:
 
-Input: [-2, -1, 0, 2, 3]
-Output: [0, 1, 4, 4, 9]
+Input: [-3, 0, 1, 2, -1, 1, -2]
+Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
+Explanation: There are four unique triplets whose sum is equal to zero.
 Example 2:
 
-Input: [-3, -1, 0, 1, 2]
-Output: [0, 1, 1, 4, 9]
+Input: [-5, 2, -1, -2, 3]
+Output: [[-5, 2, 3], [-2, -1, 3]]
+Explanation: There are two unique triplets whose sum is equal to zero.
 */
 
-// the largest squared values could be  on the ends of the array, if the array is going from negative to positive values, like [-3,-2,-1,0,1,2]. and the smaller numbers will be towards the middle. So we start from the opposite ends, using a left and right pointer. whichever squared value is bigger, we will add to our resultArray. and then we converge towards the middle, putting the greatest values first. So when we finally get to the middle number, it is the smallest value. After we are done looping we return our array in reverse, so it  goes from lowest to highest. if the array is just all positive numbers. The end value wiill always be larger, so our end pointer will keep going down till we get to the first index, at which point, we just put that value in.
-var squaringSortedArray = function squaringSortedArray(array) {
-  var left = 0;
-  var right = array.length - 1;
-  var resultArray = [];
-  while (left <= right) {
-    var leftValue = Math.pow(array[left], 2);
-    var rightValue = Math.pow(array[right], 2);
-    if (leftValue < rightValue) {
-      resultArray.push(rightValue);
-      right--;
-    } else if (leftValue >= rightValue) {
-      resultArray.push(leftValue);
-      left++;
+/* HOW DUPLICATES WORK
+
+If we have [-1, -1, -1, 0, 0, 0, 1, 1, 1] we will find a solution when current = 0, left = 3 and right = 8
+
+then if we increment left and decrement right we will find a duplicate at current = 0, left = 4 and right = 7 so instead we should increment the left pointer index until the list value != 0, and decrement the right pointer index until the list value != 1. this will only happen when left < right which finishes the while loop in this case
+
+now, if we increment current to index 1 and reset our left and right pointers we will end up with the same solution set. since the list is sorted we have already solved for when current has a list value of -1 so we apply the same principle to increment the current pointer
+*/
+
+var searchTripletsNew = function searchTripletsNew(array) {
+  // first, we sort array from lowest to highest;
+  array.sort(function (a, b) {
+    return a - b;
+  });
+  // create our variable to hold resultsArray
+  var resultsArray = [];
+  console.log(array);
+
+  // now we run a loop from beginning to end of array
+  for (var i = 0; i <= array.length - 2; i++) {
+    // if the value of array[i] that we are currently on is equal to the value of the array[i-1], then we have a duplicate number, so we want to skip this. Also if i is 0, there is no value of array[i-1] and that is why we also put the condition if i>0;
+    if (i > 0 && array[i] === array[i - 1]) {
+      continue;
+    }
+    // the targetSum of our left and rightPointers, has to be whatever value array[i] is but negative. so if array[i] =4. The sum of left and right pointers has to be -4.
+    var targetSum = -array[i];
+    // left pointer is the index after our current i
+    var leftPointer = i + 1;
+    // right pointer will be at the end of our array. Since its sorted, now its back to two pointers (left and right) which we are comfortable with.
+    var rightPointer = array.length - 1;
+
+    // left pointer has to be less than rightPointer, so that they cant ever be the same value(otherwise it wouldnt be a triplet) and also so that left pointer can't exceed right pointer, since if we find a triplet, we increase left and decrease right(so ths is where left could be greater than right, which we don't want) so we run this while loop, where we put all unique triplets in the resultsArray for each i we are on.
+    while (leftPointer < rightPointer) {
+      console.log(leftPointer);
+      console.log(rightPointer);
+      var currentSum = array[leftPointer] + array[rightPointer];
+      if (currentSum === targetSum) {
+        resultsArray.push([array[i], array[leftPointer], array[rightPointer]]);
+        // ever time we find when our currentSum equals the target we need, after pushing in all three numbers, we need to incremenet leftPointer and decrement rightPointer. Now, when we increment leftPointer, if it even exists and its value is the same as the value that came before it, we keep incrementing leftPointer. We do the same when we decrement rightPointer. This is all to avoid duplicates.
+        leftPointer++;
+        rightPointer--;
+
+        while (leftPointer < rightPointer && array[leftPointer] === array[leftPointer - 1]) {
+          leftPointer++;
+        }
+
+        while (leftPointer < rightPointer && array[rightPointer] === array[rightPointer + 1]) {
+          rightPointer--;
+        }
+      } else if (currentSum < targetSum) {
+        leftPointer++;
+      } else if (currentSum > targetSum) {
+        rightPointer--;
+      }
     }
   }
-  return resultArray.reverse();
+
+  return resultsArray;
 };
 
-console.log(squaringSortedArray([1, 2, 3, 4, 5]));
+console.log(searchTripletsNew([-3, 0, 1, 2, -1, 1, -2]));
 
 /***/ }),
 
