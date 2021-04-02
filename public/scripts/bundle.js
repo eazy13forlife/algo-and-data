ef8e0c3b777a34eb254035ab9a4d133fd347a86c
Eric <eric_baffour-addo@alumni.brown.edu>
2021-04-01 18:00:07 -0700

    almost finished with truplet sum close to target

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -15716,88 +15716,9 @@ module.exports = g;
   !*** ./source/index.js ***!
   \*************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-
-/*
-
-Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
-
-Example 1:
-
-Input: [-3, 0, 1, 2, -1, 1, -2]
-Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
-Explanation: There are four unique triplets whose sum is equal to zero.
-Example 2:
-
-Input: [-5, 2, -1, -2, 3]
-Output: [[-5, 2, 3], [-2, -1, 3]]
-Explanation: There are two unique triplets whose sum is equal to zero.
-*/
-
-/* HOW DUPLICATES WORK
-
-If we have [-1, -1, -1, 0, 0, 0, 1, 1, 1] we will find a solution when current = 0, left = 3 and right = 8
-
-then if we increment left and decrement right we will find a duplicate at current = 0, left = 4 and right = 7 so instead we should increment the left pointer index until the list value != 0, and decrement the right pointer index until the list value != 1. this will only happen when left < right which finishes the while loop in this case
-
-now, if we increment current to index 1 and reset our left and right pointers we will end up with the same solution set. since the list is sorted we have already solved for when current has a list value of -1 so we apply the same principle to increment the current pointer
-*/
-
-var searchTripletsNew = function searchTripletsNew(array) {
-  // first, we sort array from lowest to highest;
-  array.sort(function (a, b) {
-    return a - b;
-  });
-  // create our variable to hold resultsArray
-  var resultsArray = [];
-  console.log(array);
-
-  // now we run a loop from beginning to end of array
-  for (var i = 0; i <= array.length - 2; i++) {
-    // if the value of array[i] that we are currently on is equal to the value of the array[i-1], then we have a duplicate number, so we want to skip this. Also if i is 0, there is no value of array[i-1] and that is why we also put the condition if i>0;
-    if (i > 0 && array[i] === array[i - 1]) {
-      continue;
-    }
-    // the targetSum of our left and rightPointers, has to be whatever value array[i] is but negative. so if array[i] =4. The sum of left and right pointers has to be -4.
-    var targetSum = -array[i];
-    // left pointer is the index after our current i
-    var leftPointer = i + 1;
-    // right pointer will be at the end of our array. Since its sorted, now its back to two pointers (left and right) which we are comfortable with.
-    var rightPointer = array.length - 1;
-
-    // left pointer has to be less than rightPointer, so that they cant ever be the same value(otherwise it wouldnt be a triplet) and also so that left pointer can't exceed right pointer, since if we find a triplet, we increase left and decrease right(so ths is where left could be greater than right, which we don't want) so we run this while loop, where we put all unique triplets in the resultsArray for each i we are on.
-    while (leftPointer < rightPointer) {
-      console.log(leftPointer);
-      console.log(rightPointer);
-      var currentSum = array[leftPointer] + array[rightPointer];
-      if (currentSum === targetSum) {
-        resultsArray.push([array[i], array[leftPointer], array[rightPointer]]);
-        // ever time we find when our currentSum equals the target we need, after pushing in all three numbers, we need to incremenet leftPointer and decrement rightPointer. Now, when we increment leftPointer, if it even exists and its value is the same as the value that came before it, we keep incrementing leftPointer. We do the same when we decrement rightPointer. This is all to avoid duplicates.
-        leftPointer++;
-        rightPointer--;
-
-        while (leftPointer < rightPointer && array[leftPointer] === array[leftPointer - 1]) {
-          leftPointer++;
-        }
-
-        while (leftPointer < rightPointer && array[rightPointer] === array[rightPointer + 1]) {
-          rightPointer--;
-        }
-      } else if (currentSum < targetSum) {
-        leftPointer++;
-      } else if (currentSum > targetSum) {
-        rightPointer--;
-      }
-    }
-  }
-
-  return resultsArray;
-};
-
-console.log(searchTripletsNew([-3, 0, 1, 2, -1, 1, -2]));
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: \"smallestDifference\" is read-only\n\n\u001b[0m \u001b[90m 73 | \u001b[39m      \u001b[90m// here, we save the closest difference. A target difference of -1 and 1  is same. We use absolute value because the smallest differnec of -5 and 5 are the same in terms of difference.\u001b[39m\n \u001b[90m 74 | \u001b[39m      \u001b[36mif\u001b[39m (\u001b[33mMath\u001b[39m\u001b[33m.\u001b[39mabs(targetDifference) \u001b[33m<\u001b[39m \u001b[33mMath\u001b[39m\u001b[33m.\u001b[39mabs(smallestDifference)) {\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 75 | \u001b[39m        smallestDifference \u001b[33m=\u001b[39m targetDifference\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m        \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 76 | \u001b[39m      }\n \u001b[90m 77 | \u001b[39m\n \u001b[90m 78 | \u001b[39m      \u001b[90m//if the target difference is less than the smallest diffrence, save that targetDifference, but if theyre equal then only save it when the targetDifference is greater than the smallestDifference without absolute value. So that when we return our sum, which is  target-smallestDifferece smallestDifference is equal to the larger sum, we will get a smaller sum\u001b[39m\u001b[0m\n");
 
 /***/ }),
 
--- source/index.js
+++ source/index.js
@@ -1,81 +1,90 @@
 /*
-
-Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
-
-Example 1:
-
-Input: [-3, 0, 1, 2, -1, 1, -2]
-Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
-Explanation: There are four unique triplets whose sum is equal to zero.
-Example 2:
-
-Input: [-5, 2, -1, -2, 3]
-Output: [[-5, 2, 3], [-2, -1, 3]]
-Explanation: There are two unique triplets whose sum is equal to zero.
-*/
-
-/* HOW DUPLICATES WORK
-
-If we have [-1, -1, -1, 0, 0, 0, 1, 1, 1] we will find a solution when current = 0, left = 3 and right = 8
-
-then if we increment left and decrement right we will find a duplicate at current = 0, left = 4 and right = 7 so instead we should increment the left pointer index until the list value != 0, and decrement the right pointer index until the list value != 1. this will only happen when left < right which finishes the while loop in this case
-
-now, if we increment current to index 1 and reset our left and right pointers we will end up with the same solution set. since the list is sorted we have already solved for when current has a list value of -1 so we apply the same principle to increment the current pointer
-*/
-
-const searchTripletsNew = (array) => {
-  // first, we sort array from lowest to highest;
+const getSumTriplet = (array, target) => {
+  const resultArray = [];
+  let neededSum;
+  let minLength = Infinity;
+  let sumResultsInMin;
   array.sort((a, b) => a - b);
-  // create our variable to hold resultsArray
-  const resultsArray = [];
-  console.log(array);
-
-  // now we run a loop from beginning to end of array
   for (let i = 0; i <= array.length - 2; i++) {
-    // if the value of array[i] that we are currently on is equal to the value of the array[i-1], then we have a duplicate number, so we want to skip this. Also if i is 0, there is no value of array[i-1] and that is why we also put the condition if i>0;
-    if (i > 0 && array[i] === array[i - 1]) {
-      continue;
-    }
-    // the targetSum of our left and rightPointers, has to be whatever value array[i] is but negative. so if array[i] =4. The sum of left and right pointers has to be -4.
-    const targetSum = -array[i];
-    // left pointer is the index after our current i
     let leftPointer = i + 1;
-    // right pointer will be at the end of our array. Since its sorted, now its back to two pointers (left and right) which we are comfortable with.
     let rightPointer = array.length - 1;
+    const firstValue = array[i];
 
-    // left pointer has to be less than rightPointer, so that they cant ever be the same value(otherwise it wouldnt be a triplet) and also so that left pointer can't exceed right pointer, since if we find a triplet, we increase left and decrease right(so ths is where left could be greater than right, which we don't want) so we run this while loop, where we put all unique triplets in the resultsArray for each i we are on.
+    if (firstValue >= 0) {
+      neededSum = target - firstValue;
+    } else {
+      neededSum = target + firstValue * -1;
+    }
+
     while (leftPointer < rightPointer) {
-      console.log(leftPointer);
-      console.log(rightPointer);
       const currentSum = array[leftPointer] + array[rightPointer];
-      if (currentSum === targetSum) {
-        resultsArray.push([array[i], array[leftPointer], array[rightPointer]]);
-        // ever time we find when our currentSum equals the target we need, after pushing in all three numbers, we need to incremenet leftPointer and decrement rightPointer. Now, when we increment leftPointer, if it even exists and its value is the same as the value that came before it, we keep incrementing leftPointer. We do the same when we decrement rightPointer. This is all to avoid duplicates.
-        leftPointer++;
+      if (currentSum > neededSum) {
         rightPointer--;
-
-        while (
-          leftPointer < rightPointer &&
-          array[leftPointer] === array[leftPointer - 1]
-        ) {
-          leftPointer++;
+        if (leftPointer === rightPointer) {
+          resultArray.push(
+            array[i] + array[leftPointer] + array[rightPointer + 1]
+          );
         }
-
-        while (
-          leftPointer < rightPointer &&
-          array[rightPointer] === array[rightPointer + 1]
-        ) {
-          rightPointer--;
-        }
-      } else if (currentSum < targetSum) {
+      } else if (currentSum < neededSum) {
         leftPointer++;
-      } else if (currentSum > targetSum) {
-        rightPointer--;
+        if (leftPointer === rightPointer) {
+          resultArray.push(
+            array[i] + array[leftPointer - 1] + array[rightPointer]
+          );
+        }
+      } else if (currentSum === neededSum) {
+        return target;
+      }
+    }
+    console.log(resultArray);
+  }
+
+  for (let i = 0; i < resultArray.length; i++) {
+    let difference = array[i] > 0 ? target - array[i] : target + array[i] * -1;
+    minLength = Math.min(minLength, difference);
+    if (minLength === difference) {
+      sumResultsInMin = resultArray[i];
+    }
+  }
+  return sumResultsInMin;
+};
+*/
+
+const getTripletSum = (array, target) => {
+  array.sort((a, b) => a - b);
+  const smallestDifference = Infinity;
+  for (let i = 0; i < array.length - 2; i++) {
+    let leftPointer = i + 1;
+    let rightPointer = array.length - 1;
+    const firstValue = array[i];
+
+    if (firstValue >= 0) {
+      neededSum = target - firstValue;
+    } else {
+      neededSum = target + firstValue * -1;
+    }
+    while (leftPointer < rightPointer) {
+      //so we have our targetNumber and we subtract all of our values to see the difference from the targetNumber. A difference of -1 and 1 is the same btw. So, we have to do Math.abs(targetDifference) later on.
+      const targetDifference = target - (array[i] + array[left] + array[right]); // subtracting negative and positive is same. just subtract. Same with adding, just add.
+      if (targetDifference === 0) {
+        return target; //if the difference is 0,this sum is the exact same as the targetNumber, so  just return the sum of all the numbers, which is the target.
+      }
+
+      // here, we save the closest difference. A target difference of -1 and 1  is same. We use absolute value because the smallest differnec of -5 and 5 are the same in terms of difference.
+      if (Math.abs(targetDifference) < Math.abs(smallestDifference)) {
+        smallestDifference = targetDifference;
+      }
+
+      //if the target difference is less than the smallest diffrence, save that targetDifference, but if theyre equal then only save it when the targetDifference is greater than the smallestDifference without absolute value. So that when we return our sum, which is  target-smallestDifferece smallestDifference is equal to the larger sum, we will get a smaller sum
+      if (
+        Math.abs(targetDifference) < Math.abs(smallestDfference) ||
+        (Math.abs(targetDfference) === Math.abs(smallestDifference) &&
+          targetDifference > smallestDifference)
+      ) {
+        smallestDifference = targetDifference;
       }
     }
   }
-
-  return resultsArray;
+  return target - smallestDifference;
 };
-
-console.log(searchTripletsNew([-3, 0, 1, 2, -1, 1, -2]));
+console.log(getSumTriplet([-3, -1, 1, 2], 1));
