bd126ab221573985fca464fbfd9b06a9f2639da4
Eric <eric_baffour-addo@alumni.brown.edu>
2021-07-08 00:42:28 -0700

    finished cpu time

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -15721,6 +15721,69 @@ module.exports = g;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var MinHeap = function () {
+  function MinHeap() {
+    _classCallCheck(this, MinHeap);
+
+    this.heap = [];
+  }
+
+  _createClass(MinHeap, [{
+    key: "insert",
+    value: function insert(value) {
+      this.heap.push(value);
+      this.heap.sort(function (a, b) {
+        return a[1] - b[1];
+      });
+    }
+  }, {
+    key: "extractMin",
+    value: function extractMin() {
+      this.heap.shift();
+    }
+  }]);
+
+  return MinHeap;
+}();
+
+var getMaxCPU = function getMaxCPU(intervalsArray) {
+  intervalsArray.sort(function (a, b) {
+    return a[0] - b[0];
+  });
+
+  var heap = new MinHeap();
+  heap.insert(intervalsArray[0]);
+  var maxSize = intervalsArray[0][2];
+  var currentSize = intervalsArray[0][2];
+
+  console.log(maxSize);
+
+  for (var i = 1; i < intervalsArray.length; i++) {
+    var interval = intervalsArray[i];
+    //while there is something in our heap and the earliest end time in our heap is less than or equal to our current intervals start time, it means that interval is done so we can remove it from our heap, which contains the current jobs happening
+    while (heap.heap.length && heap.heap[0][1] <= interval[0]) {
+      //lets remove this cpu time from our current maxSize
+      currentSize = currentSize - heap.heap[0][2];
+
+      //lets actually remove this item from our heap
+      heap.extractMin();
+    }
+    //not we can insert our interval to our heap. so what we did is we have finsihed removing all of the jobs that have finished at the start of this interval, and so now our heap contains the current jobs actually happening.
+    heap.insert(interval);
+    //lets recalculate maxSize since we added in a new interval to our heap
+    currentSize = currentSize + interval[2];
+    //lets push in this maxSize to our maxSizes array
+    maxSize = Math.max(currentSize, maxSize);
+  }
+  return maxSize;
+};
+
+console.log(getMaxCPU([[1, 4, 2], [2, 4, 1], [3, 6, 5]]));
+
 /***/ }),
 
 /***/ 0:
--- source/Udemy/patterns/4.mergeIntervals/5.maxCPU.js
+++ source/Udemy/patterns/4.mergeIntervals/5.maxCPU.js
@@ -25,11 +25,9 @@ const getMaxCPU = (intervalsArray) => {
   let maxSize = intervalsArray[0][2];
   let currentSize = intervalsArray[0][2];
 
-  console.log(maxSize);
-
   for (let i = 1; i < intervalsArray.length; i++) {
     let interval = intervalsArray[i];
-    //while there is something in our heap and the earliest end time in our heap is less than or equal to our current intervals start time, it means that interval is done so we can remove it from our heap, which contains the current jobs happening
+    //while there is something in our heap and the earliest end time in our heap is less than or equal to our current intervals start time, it means that interval is done so we can remove it from our heap, which contains the current jobs happening at the moment this interval starts
     while (heap.heap.length && heap.heap[0][1] <= interval[0]) {
       //lets remove this cpu time from our current maxSize
       currentSize = currentSize - heap.heap[0][2];
@@ -37,11 +35,11 @@ const getMaxCPU = (intervalsArray) => {
       //lets actually remove this item from our heap
       heap.extractMin();
     }
-    //not we can insert our interval to our heap. so what we did is we have finsihed removing all of the jobs that have finished at the start of this interval, and so now our heap contains the current jobs actually happening.
+    //not we can insert our interval to our heap. so what we did is we have finsihed removing all of the jobs that have finished at the start of this interval, and so now our heap contains the current jobs actually happening at the start of this interval
     heap.insert(interval);
     //lets recalculate maxSize since we added in a new interval to our heap
     currentSize = currentSize + interval[2];
-    //lets push in this maxSize to our maxSizes array
+    //lets find the maxSize thus far
     maxSize = Math.max(currentSize, maxSize);
   }
   return maxSize;
